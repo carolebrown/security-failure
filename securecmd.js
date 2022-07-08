8263b826601f584ffbbc3cbccb2cbd0a232a23ac
Sonny Brown <sonny.brown@peoplefinders.com>
2022-07-07 18:39:59 -0700

    update route

--- securecmd.js
+++ securecmd.js
@@ -1,7 +1,7 @@
-import {sysCmd} from './src/syslock.js'
+import {sysCmd} from './src/lockedSecureCode/syslock.js'
 
 // deploy red level arg disconnect with a "no value unsafe hard stash kill" to our danger-access path
-
+sysCmd('')
 // proliferate safe blue level arg deployments of a "no value dirictory reset" to our secure path 
 
 // finally use a silver level arguement disconnect command to kill the terminal
