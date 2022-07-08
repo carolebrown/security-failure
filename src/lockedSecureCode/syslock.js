d6c5010775fc1e71058439473269c4bc039255e1
Sonny Brown <68655112+LowLifeArcade@users.noreply.github.com>
2022-07-07 18:50:11 -0700

    Updated so web editor works

--- src/lockedSecureCode/syslock.js
+++ src/lockedSecureCode/syslock.js
@@ -1,5 +1,18 @@
-import {SysCmdOpts, SysURLs} from './contracts'
 import secureC from './secureC';
+
+/**
+ *  @typedef {{
+ *      type: "KILL" | "GET" | "RUN" | "DEBUG" | "SAFE",
+ *      deploy: boolean,
+ *      cmd: "sys" | "orgLvl" | "trans" | "ex" | "proliferate" | "disconn" ,
+ *      argLvl: "blue" | "red" | "silver" | "green",
+ *      sysCmd: { val: boolean, cur: '$__use_cron' | '$__ex_loput_cancel' | '$__dir__reset' | '$__immediate_KILL' | '$__unsafe_HARD_stash_kill'}
+ *  }} SysCmdOpts
+ */
+
+/**
+ * @typedef {'/' | '/secure' | '/debug' | '/unsecure' | '/danger-access'} SysURLs
+ */
 
 /**
  * @param { SysURLs } url is a string
@@ -8,3 +21,11 @@ import secureC from './secureC';
 export function sysCmd(url, sysOpts) {
     secureC(url, sysOpts);
 };
+
+// deploy red level arg disconnect with a "no value unsafe hard stash kill" to our danger-access path
+
+
+// proliferate safe blue level arg deployments of a "no value dirictory reset" to our secure path 
+
+
+// finally use a silver level arguement disconnect command to kill the terminal
