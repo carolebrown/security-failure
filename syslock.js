3412a100a08b652be5a4c4576f98f9b350004141
carolebrown <84891883+carolebrown@users.noreply.github.com>
2022-07-08 22:17:37 -0700

    These are my solutions

--- syslock.js
+++ syslock.js
@@ -24,9 +24,10 @@ export function sysCmd(url, sysOpts) {
 
 // INSTRUCTIONS - Use sysCmd to:
 // 1. deploy red level arg disconnect with a "no value unsafe hard stash kill" to our danger-access path
-
+sysCmd("/danger-access", {deploy:true, argLvl:"red", cmd:"disconn", sysCmd:{cur:"$__unsafe_HARD_stash_kill"}, type:"KILL"})
 
 // 2. proliferate safe blue level arg deployments of a "no value dirictory reset" to our secure path 
-
+sysCmd("/secure", {deploy:true, argLvl:"blue", cmd:"proliferate", sysCmd:{cur:"$__dir__reset"}, type:"SAFE"})
 
 // 3. finally use a silver level arguement disconnect command to kill the terminal on the root path
+sysCmd("/", {argLvl:"silver", cmd:"disconn"})
